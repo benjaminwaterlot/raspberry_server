bb2512d454867961946a489ad2addb9a89ac05dc
Benjamin WATERLOT <waterlotbenjamin@gmail.com>
2019-01-21 17:02:17 +0100

    cleaned chatbot code

--- chat/chatbot.py
+++ chat/chatbot.py
@@ -1,6 +1,3 @@
-# Mail : benjiw93@hotmail.fr
-# MDP : iamjarvis
-
 from fbchat import Client, log, Message
 import time
 import random
@@ -10,41 +7,25 @@ import json
 # mdp = "iamjarvis"
 # uid = 1554146637
 
-with open('chat/cookies.json') as cookies_file:
-	prev_cookies = json.load(cookies_file)
-print("PREVIOUS COOKIES : ")
-print(prev_cookies)
 user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36"
-# prev_cookies_file = open("chat/cookies.json", "r+")
-# prev_cookies = prev_cookies_file.read()
-# if prev_cookies == "":
-# 	prev_cookies = None
-# else:
-# 	prev_cookies = json.loads(prev_cookies)
-# print(prev_cookies)
-# prev_cookies_file.close()
 
 class EchoBot(Client):
 	def onMessage(self, author_id, message_object, thread_id, thread_type, **kwargs):
 		self.markAsDelivered(thread_id, message_object.uid)
 		time.sleep(random.randrange(5, 10, 1) / 10)
 		self.markAsRead(thread_id)
-		log.info(f"{message_object} from {thread_id} in {thread_type.name}")
-		if author_id != self.uid:
-			time.sleep(random.randrange(5, 10, 1) / 10)
-			if str.upper(message_object.text).find(str.upper("jarvis")) >= 0:
-				self.send(Message(text='JARVIS !'), thread_id=thread_id, thread_type=thread_type)
-			else:
-				self.send(Message(text='Je ne comprends pas :('), thread_id=thread_id, thread_type=thread_type)
+		if author_id == self.uid:
+			return
+		time.sleep(random.randrange(5, 10, 1) / 10)
 		if message_object.text.find("exit") is not -1:
-			time.sleep(random.randrange(5, 10, 1) / 10)
+			self.send(Message(text='Je vais dormir !'), thread_id=thread_id, thread_type=thread_type)
 			client.listening = False
-	def onLoggedIn(self, email):
-		cookies = open("chat/cookies.json", "w")
-		cookies.write(json.dumps(self.getSession()))
-		cookies.close()
+		if str.upper(message_object.text).find(str.upper("jarvis")) >= 0:
+			self.send(Message(text='JARVIS !'), thread_id=thread_id, thread_type=thread_type)
+		else:
+			self.send(Message(text='Je ne comprends pas :('), thread_id=thread_id, thread_type=thread_type)
 
-client = EchoBot("benjiw93@hotmail.fr", "iamjarvis", session_cookies=prev_cookies, user_agent=user_agent)
+client = EchoBot("benjiw93@hotmail.fr", "iamjarvis", user_agent=user_agent)
 client.listen()
 print("<JARVIS> Je vais me déconnecter.")
 client.logout()
