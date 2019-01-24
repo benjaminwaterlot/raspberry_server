cd1bbafe42e3b15f9c7ded89039c45d1207c083c
Benjamin WATERLOT <waterlotbenjamin@gmail.com>
2019-01-24 13:31:18 +0100

    working omg

--- app.py
+++ app.py
@@ -6,7 +6,7 @@
 #    By: bwaterlo <bwaterlo@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/01/22 11:56:22 by bwaterlo          #+#    #+#              #
-#    Updated: 2019/01/24 11:25:30 by bwaterlo         ###   ########.fr        #
+#    Updated: 2019/01/24 13:23:58 by bwaterlo         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,11 +15,28 @@ import requests
 import json
 import trainline.trainline as trainline
 import datetime as dt
+from requests_futures.sessions import FuturesSession
 
 app = Flask(__name__)
 
 PAGE_TOKEN = "EAALSRXE5200BAJZAZBoM9YEUZCtm429dVIXWe1EIh3U0IKKB1Nb2evh5vtjj2ZA1CRnBkBnZCWynzaaiBupJ8MB7S7Q2fjyDQT3RNMcxIHbiWxHf1Tqx8TrtwihGyOZAWodC5aGRbYcB1QeYlClE5mqFsbCHbVfZAD8f5s0JmszLgZDZD"
 VERIFY_TOKEN = "coucou"
+
+session_headers = {
+	"accept-language": "fr-FR,fr;q=0.8",
+	"authorization": "Token token=\"bn1jmsKj7-EzsyRrx4bt\"",
+	"x-ct-client-id": "91f690ff-8005-4203-9145-91567ca4a656",
+	"x-ct-version": "8381c2c803382d9064fdd08cf188cf28488da811",
+	"x-ct-locale": "fr",
+	"x-user-agent": "CaptainTrain/1546957175(web) (Ember 3.4.6)",
+	"x-requested-with": "XMLHttpRequest",
+	"x-not-a-bot": "i-am-human",
+	"user-agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+	"referer": "https://www.trainline.eu/search",
+	"authority": 'www.trainline.eu',
+	"accept": "application/json,text/javascript, */*; q=0.01",
+	"content-type": "application/json; charset=UTF-8",
+}
 
 @app.route('/bot', methods=['GET'])
 def handle_verification():
@@ -31,28 +48,12 @@ def handle_verification():
 		return "ERROR"
 
 
-# @app.route('/bot', methods=['POST'])
-# def handle_message():
-# 	data = request.get_json()
-# 	print("MESSAGE RECEIVED : ")
-# 	print(data)
-# 	if data['object'] == 'page':
-# 		for entry in data['entry']:
-# 			for messaging_event in entry['messaging']:
-# 				if messaging_event.get('message'):
-# 					sender_id = messaging_event['sender']['id']
-# 		requests.post("https://graph.facebook.com/v2.6/me/messages",
-# 			params={"access_token": PAGE_TOKEN},
-# 			headers={"Content-Type": "application/json"},
-# 			data= json.dumps({
-# 				"recipient": {"id": sender_id},
-# 				"message": {"text": "JARVIS !"}
-# 			}))
-# 	return "Okay, bro !"
-
 @app.route('/webhook', methods=['POST'])
 def message_from_jarvis():
 	body = request.get_json()
+	session = FuturesSession()
+	# session = requests.Session()
+	session.headers.update(session_headers)
 	print("THIS IS THE REQUEST RECEIVED :")
 	print(body)
 	with open('logs/request_from_dialogflow.json', 'w+') as log:
@@ -69,53 +70,17 @@ def message_from_jarvis():
 			'time_start': dt.datetime.fromisoformat(query_params['time_period']['startTime']).time(),
 			'time_end': dt.datetime.fromisoformat(query_params['time_period']['endTime']).time(),
 		}
-		# response = {}
-		# response['fulfillmentText'] = trainline.search(params)
-		response = {
-			"fulfillmentMessages": [
-			{
+		response = {}
+		trains_found = trainline.search(session, params)
+		if len(trains_found) == 0:
+			response['fulfillmentText'] = "Désolé, je n'ai rien trouvé :("
+		else:
+			response['fulfillmentMessages'] = [{
 				"platform": "FACEBOOK",
-				"card": {
-					"title": "Title: this is a title",
-					"subtitle": "This is an subtitle.  Text can include unicode characters including emoji 📱.",
-					"imageUri": "https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png",
-					"buttons": [
-					{
-						"text": "This is a button",
-						"postback": "https://assistant.google.com/"
-					}
-					]
+				"quick_replies": {
+					'title': f"Voilà, j'en ai trouvé {len(trains_found)} !",
+					"quickReplies": trains_found
 				}
-			},
-			{
-			"platform": "FACEBOOK",
-			"card": {
-				"title": "Title: this is a title",
-				"subtitle": "This is an subtitle.  Text can include unicode characters including emoji 📱.",
-				"imageUri": "https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png",
-				"buttons": [
-				{
-					"text": "This is a button",
-					"postback": "https://assistant.google.com/"
-				}
-				]
-			}
-			}
-		]
-		}
-
-		# response['fulfillmentMessages'] = [{'messages': [{
-		# 		"buttons": [
-		# 		{
-		# 			"postback": "Card Link URL or text",
-		# 			"text": "Card Link Title"
-		# 		}
-		# 		],
-		# 		"imageUrl": "http://urltoimage.com",
-		# 		"platform": "facebook",
-		# 		"subtitle": "Card Subtitle",
-		# 		"title": "Card Title",
-		# 		"type": 1
-		# 	}]}]
+			}]
 		return json.dumps(response)
 	else: exit()
--- trainline/trainline.py
+++ trainline/trainline.py
@@ -6,7 +6,7 @@
 #    By: bwaterlo <bwaterlo@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/01/15 10:37:06 by bwaterlo          #+#    #+#              #
-#    Updated: 2019/01/22 19:24:00 by bwaterlo         ###   ########.fr        #
+#    Updated: 2019/01/24 13:28:01 by bwaterlo         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -18,27 +18,17 @@ import datetime as dt
 
 import logs
 from stations import stations_db
+from resources import months
 
 search_url = "https://www.trainline.eu/api/v5_1/search"
+
 
 def get_search_headers():
 	search_headers = {
 		'x-ct-timestamp': str(dt.time()),
-		"accept-language": "fr-FR,fr;q=0.8",
-		"authorization": "Token token=\"bn1jmsKj7-EzsyRrx4bt\"",
-		"x-ct-client-id": "91f690ff-8005-4203-9145-91567ca4a656",
-		"x-ct-version": "8381c2c803382d9064fdd08cf188cf28488da811",
-		"x-ct-locale": "fr",
-		"x-user-agent": "CaptainTrain/1546957175(web) (Ember 3.4.6)",
-		"x-requested-with": "XMLHttpRequest",
-		"x-not-a-bot": "i-am-human",
-		"user-agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
-		"referer": "https://www.trainline.eu/search",
-		"authority": 'www.trainline.eu',
-		"accept": "application/json,text/javascript, */*; q=0.01",
-		"content-type": "application/json; charset=UTF-8",
 	}
 	return search_headers
+
 
 def get_search_body(params):
 	departure_date = params['date']
@@ -65,6 +55,7 @@ def get_search_body(params):
 	}
 	return {"search": search_body}
 
+
 def format_result(hours):
 	array = []
 	result = ""
@@ -73,6 +64,7 @@ def format_result(hours):
 		array += '\n'
 	return result.join(array)
 
+
 def save_to_logs(trains_all, trains_tgvmax):
 	logs.debug("Saving the results to disk")
 	with open("results/trains_all.json", "w+") as trains_all_file:
@@ -80,30 +72,33 @@ def save_to_logs(trains_all, trains_tgvmax):
 	with open("results/trains_tgvmax.json", "w+") as trains_tgvmax_file:
 		trains_tgvmax_file.write(json.dumps(trains_tgvmax))
 
+
 def log_trains(trains_all, trains_tgvmax, trains_buyable):
 	logs.message(f"{len(trains_all)} TRAINS FOUND.")
 	logs.message(f"{len(trains_tgvmax)} TGVMAX FOUND.")
 	logs.message(f"{len(trains_buyable)} AVAILABLE TGVMAX FOUND.")
 	for train in trains_tgvmax:
-		logs.message(f"> {train['departure_date']} for {str(train['cents'] / 100)}€.")
+		logs.message(f"> {str(train['departure_date'])} for {str(train['cents'] / 100)}€.")
 
-def search(params):
-	def launch_search(params):
-		print("WILL SEND THIS BODY : ")
-		print(get_search_body(params))
-		search_response = requests.post(search_url, json=get_search_body(params), headers=get_search_headers())
-		search_response.raise_for_status()
-		trains_all = search_response.json()['folders']
-		trains_tgvmax = [train for train in trains_all if train['cents'] == 0]
-		trains_buyable = [train for train in trains_tgvmax if train['is_sellable'] == True]
-		keep_hours = [train['departure_date'] for train in trains_buyable]
-		log_trains(trains_all, trains_tgvmax, trains_buyable)
-		save_to_logs(trains_all, trains_tgvmax)
-		return format_result(keep_hours)
-	result = ""
+
+def search(session, params):
+	result = []
+	future_requests = []
 	while params['time_start'] < params['time_end']:
-		result += launch_search(params)
+		future_requests.append(session.post(search_url, json=get_search_body(params), headers=get_search_headers()))
 		if params['time_start'].hour >= 21:
 			break
 		params['time_start'] = params['time_start'].replace(hour=params['time_start'].hour + 3)
-	return f"TGVMAX found: \n{result}" if result else "NOTHING!\n"
+	
+	for request in future_requests:
+		response = request.result()
+		response.raise_for_status()
+		trains_all = response.json()['folders']
+		trains_tgvmax = [train for train in trains_all if train['cents'] == 0]
+		trains_buyable = [train for train in trains_tgvmax if train['is_sellable'] == True]
+		keep_hours = [dt.datetime.fromisoformat(train['departure_date']) for train in trains_buyable]
+		log_trains(trains_all, trains_tgvmax, trains_buyable)
+		trains = [f"{d.hour}h{d.minute} ({d.day} {months.months[d.month]})" for d in keep_hours]
+		for train in trains:
+			result.append(train)
+	return result
